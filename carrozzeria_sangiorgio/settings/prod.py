9da3d63a8e90c026bd1dd4eb42aeb130ea6e937e
ishrid <ahmad.ishrid@gmail.com>
2025-06-30 17:45:50 +0200

    Add newsssi

--- carrozzeria_sangiorgio/settings/prod.py
+++ carrozzeria_sangiorgio/settings/prod.py
@@ -1,5 +1,3 @@
-# carrozzeria_sangiorgio/settings/prod.py
-
 import os
 from .base import *
 import dj_database_url
@@ -11,26 +9,28 @@ INSTALLED_APPS += [
     'cloudinary',
 ]
 
-# Inserisci qui il dominio/host effettivo del tuo progetto in produzione
 ALLOWED_HOSTS = ['web-production-6298c6.up.railway.app', 'localhost', '127.0.0.1']
 
-# Configurazione database PostgreSQL via DATABASE_URL
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
-# Configurazione Cloudinary (variabili d'ambiente)
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
     'API_KEY': os.environ.get('API_KEY'),
     'API_SECRET': os.environ.get('API_SECRET'),
 }
 
-# Imposta Cloudinary come storage per media e static in produzione
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-#STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
+# Nuova sintassi per Django 5+
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
-# STATIC_ROOT deve essere definito per collectstatic
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Sicurezza HTTPS
