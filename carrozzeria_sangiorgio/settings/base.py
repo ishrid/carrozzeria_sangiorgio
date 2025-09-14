653b2c4e17537f1df163ee3273ba12d88393fe3f
ishrid <ahmad.ishrid@gmail.com>
2025-09-14 18:46:13 +0200

    asdfaskkplloooo

--- carrozzeria_sangiorgio/settings/base.py
+++ carrozzeria_sangiorgio/settings/base.py
@@ -64,10 +64,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'it-ch'
 TIME_ZONE = 'UTC'
 USE_I18N = True
-USE_TZ = True
+USE_TZ = True 
 
 
 
